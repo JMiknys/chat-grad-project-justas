6802454a4bc1cf79a49bdb011c25dadb44cc9fdb
Justas Miknys <jmiknys@scottlogic.co.uk>
2016-08-22 08:31:21 +0100

    added online status for users

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+/*globals socket, alert, console */
 (function() {
     var app = angular.module("ChatApp", []);
 
@@ -10,9 +11,15 @@
         // Thats where loaded conversations are stored
         $scope.conversations = [];
 
+        $scope.onlineUsers = {};
+
         $http.get("/api/user").then(function(userResult) {
             $scope.loggedIn = true;
             $scope.user = userResult.data;
+
+            // Register userID with the server
+            socket.emit("register_user", $scope.user._id);
+
             $http.get("/api/users").then(function(result) {
                 $scope.users = result.data;
             });
@@ -37,17 +44,43 @@
             });
 
             data.title = $scope.title;
-            data.messages = [{sender: $scope.user._id, time: Date.now(), body: "Hello, I would like to start a conversation."}];
+            data.messages = [{sender: $scope.user._id, time: Date.now(), body: "Hello"}];
 
-            socket.emit('new_conversation', data);
-            alert(JSON.stringify(data));
+            socket.emit("new_conversation", data);
+            //alert(JSON.stringify(data));
 
             $scope.title = "";
             $scope.chat = [];
         };
 
         this.sendMessage = function (conversation, msg) {
-            alert(conversation+ " ID. Trying to send message: "+msg);
+            var data = {};
+            data.id = conversation;
+            data.body = msg;
+            data.sender = $scope.user._id;
+            data.time = Date.now();
+            socket.emit("message", data);
+
+            // Remove input from textbox
+            document.getElementById(conversation + "-input").value = "";
+        };
+
+        this.leaveConversation = function (conv_id) {
+            var data = {};
+            data.userId = $scope.user._id;
+            data.conversation = conv_id;
+            socket.emit("leaveConversation", data);
+        };
+
+        this.showOnline = function () {
+            alert(JSON.stringify($scope.onlineUsers));
+        };
+
+        this.isOnline = function(userId) {
+            if ($scope.onlineUsers[userId]) {
+                return true;
+            }
+            return false;
         };
 
         this.getConversations = function () {
@@ -60,8 +93,36 @@
             console.info(msg);
         });
 
-        socket.on("response", function (msg) {
-            console.info(msg);
+        socket.on("message", function(msg) {
+            console.log("Server received a new message: " + JSON.stringify(msg));
+            // append message to correct conversation
+            $scope.conversations.filter(function (conv) {
+                return conv._id === msg.id;
+            })[0].messages.push(msg);
+            $scope.$apply();
+
+            // scroll to bottom
+            var div = document.getElementById(msg.id + "-messages");
+            div.scrollTop = div.scrollHeight;
+        });
+
+        socket.on("users", function(data) {
+            console.log("Received a list of users from server!");
+            console.log(data);
+            $scope.onlineUsers = data;
+            $scope.$apply();
+        });
+
+        socket.on("user-disconnect", function (userId) {
+            console.log("User disconnected. MSG: " + userId);
+            delete $scope.onlineUsers[userId];
+            $scope.$apply();
+        });
+
+        socket.on("user-connect", function (userId) {
+            console.log("User connected. MSG: " + userId);
+            $scope.onlineUsers[userId] = "online";
+            $scope.$apply();
         });
     });
 })();
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+/*globals Map, socket */
 var express = require("express");
 var cookieParser = require("cookie-parser");
 
@@ -13,7 +14,9 @@ module.exports = function(port, db, githubAuthoriser) {
     // Added variables by Justas
     var conversations = db.collection("conversations-justas");
     var io = require("socket.io").listen(9999);
-    var clients = [];
+    var clients = new Map();
+    var onlineUsers = {};
+    var ObjectID = require("mongodb").ObjectID;
 
     app.get("/oauth", function(req, res) {
         githubAuthoriser.authorise(req, function(githubUser, token) {
@@ -90,51 +93,82 @@ module.exports = function(port, db, githubAuthoriser) {
             }
         });
     });
-    /*
-    app.get("/api/conversations", function (req, res) {
-        res.json({
-          justas: "justas is amazing!"
-        });
 
-        conversations.insertOne({
-            participants: [{id: "JMiknys"}, {id: "TestUser"}],
-            messages: [{sender: "JMiknys", time: 1049, body:"first message"}]
-        });
-    });
+    io.on("connection", function(socket) {
+        console.log("New client connected ID: " + socket.id);
+        clients.set(socket, null);
 
-    app.post("/api/conversations", function (req, res) {
-        var conversation = req.body;
-        console.log(conversation);
-        res.sendStatus(201);
-    });
-    */
+        socket.on("disconnect", function() {
+            var disconnectedUser = clients.get(socket);
+            console.log("Client disconnected: " + disconnectedUser);
+            delete onlineUsers[disconnectedUser];
+            clients.delete(socket);
 
-    io.on('connection', function(socket) {
-        console.log("New client connected ID: "+socket.id);
-        clients.push(socket);
+            // TODO SEND EVERYONE USER DISCONNECTED MESSAGE!
+            io.sockets.emit("user-disconnect", disconnectedUser);
 
-        socket.on('disconnect', function() {
-            var index = clients.indexOf(socket);
-            if (index != -1) {
-                clients.splice(index,1);
-                console.log('Client disconnected: '+socket.id);
-            }
         });
 
         // New conversation request
-        socket.on('new_conversation', function(msg) {
-            console.log('Received a new conversation request: ' + JSON.stringify(msg));
+        socket.on("new_conversation", function(msg) {
+            console.log("Received a new conversation request: " + JSON.stringify(msg));
             conversations.insertOne(msg);
         });
 
         // Get conversations
-        socket.on('init_conversations', function(clientId) {
-            console.log("Client asked to get conversations. Asker: "+clientId);
-            var results = conversations.find({'participants.id': 'JMiknys'}).toArray(function (err, items) {
-                console.log("Found "+items.length+" conversations. Sending back answer");
+        socket.on("init_conversations", function(clientId) {
+            console.log("Client asked to get conversations. Asker: " + clientId);
+            var results = conversations.find({"participants.id": clientId}).toArray(function (err, items) {
+                console.log(JSON.stringify(items));
                 socket.emit("init_conversations", items);
+                // Subscribe to all rooms
+                items.forEach(function (el) {
+                    socket.join(el._id);
+                });
             });
         });
+
+        socket.on("message", function(msg) {
+            console.log("Received a new message request: " + JSON.stringify(msg));
+            //conversations.insertOne(msg);
+            //var cnv = conversations.find(ObjectId(msg.id));
+            conversations.updateOne(
+              {_id: new ObjectID(msg.id)},
+              {$push: {"messages": {
+                      sender: msg.sender,
+                      time: msg.time,
+                      body: msg.body}
+                     }
+              }, function(err, data) {
+                  console.log("Message was sucessfully added to DB");
+                  // Tell other listeners about Message
+                  io.to(msg.id).emit("message", msg);
+              });
+        });
+
+        socket.on("leaveConversation", function(data) {
+            console.log("Leave request received: " + JSON.stringify(data));
+            conversations.updateOne(
+            {_id: new ObjectID(data.conversation)},
+            {$pull : {"participants": {id: data.userId}}
+          }, function(err, data) {
+              // callback after user is deleted from conversation
+          });
+
+        });
+
+        socket.on("register_user", function(userId) {
+            clients.set(socket, userId);
+            onlineUsers[userId] = "online";
+            console.log("user registered: " + clients.get(socket));
+
+            // send the client a list of other registered users
+            socket.emit("users", onlineUsers);
+
+            // TODO SEND OTHER CLIENTS USER CONNECTED MESSAGE!
+            socket.broadcast.emit("user-connect", userId);
+        });
+
     });
 
     return app.listen(port);
